f547ed4205a67db82e1ee086450dce175b7893fd
blong7976 <2456399912@qq.com>
2019-08-31 12:53:56 +0800

    Signed-off-by: blong7976 <2456399912@qq.com>

---
+++ pages/tabbar/tabbar1/tabbar1.vue
@@ -0,0 +1,149 @@
+<template>
+	<view class="content">
+		<view class="uni-padding-wrap">
+            <view class="page-section swiper">
+                <view class="page-section-spacing">
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+                        <swiper-item>
+                            <view class="swiper-item uni-bg-red">A</view>
+                        </swiper-item>
+                        <swiper-item>
+                            <view class="swiper-item uni-bg-green">B</view>
+                        </swiper-item>
+                        <swiper-item>
+                            <view class="swiper-item uni-bg-blue">C</view>
+                        </swiper-item>
+                    </swiper>
+                </view>
+            </view>
+			
+			<view class="page-section mt section1">
+				<view class="heade">人脉加粉</view>
+				<view class="list">
+					<view class="item" v-for="(v,index) in 6" :key="index" @click="navigatorto(index)">
+						<image src="../../../static/img/release.png" mode="" class="icon"></image>
+						<view class="title">
+							通信录加粉
+						</view>
+						<view class="describe">
+							自动添加微信推荐的手机联系人
+						</view>
+					</view>
+				</view>
+			</view>
+			
+			<view class="page-section mt section1">
+				<view class="heade">转发点赞</view>
+				<view class="list">
+					<view class="item" v-for="(v,index) in 3" :key="index" @click="navigatorto(index)">
+						<image src="../../../static/img/release.png" mode="" class="icon"></image>
+						<view class="title">
+							通信录加粉
+						</view>
+						<view class="describe">
+							自动添加微信推荐的手机联系人
+						</view>
+					</view>
+				</view>
+			</view>
+			
+			<view class="page-section mt section1">
+				<view class="heade">群发消息</view>
+				<view class="list">
+					<view class="item" v-for="(v,index) in 3" :key="index" @click="navigatorto(index)">
+						<image src="../../../static/img/release.png" mode="" class="icon"></image>
+						<view class="title">
+							通信录加粉
+						</view>
+						<view class="describe">
+							自动添加微信推荐的手机联系人
+						</view>
+					</view>
+				</view>
+			</view>
+        </view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			background: ['color1', 'color2', 'color3'],
+            indicatorDots: true,
+            autoplay: true,
+            interval: 2000,
+            duration: 500
+		};
+	},
+	onLoad() {},
+	methods: {
+		navigatorto(v){
+			console.log(v)
+		},
+	},
+	onPullDownRefresh() {
+		setTimeout(function () {
+            uni.stopPullDownRefresh();
+        }, 1000);
+	},
+
+};
+</script>
+
+<style lang="scss" scoped>
+page{background-color: #f5f5f5;}
+.content {
+	.swiper{
+		background-color: #007AFF;
+		text-align: center;	
+	}
+	.mt{
+		margin-top: 20upx;
+		padding: 20upx;
+		box-sizing: border-box;
+	}
+	.section1{
+		background-color: #fff;
+		.heade{
+			font-size: 32upx;
+			position: relative;
+			padding-left: 20upx;
+		}
+		.heade::after{
+			content: "";
+			width: 4upx;
+			height: 32upx;
+			background-color: #007AFF;
+			position: absolute;
+			left: 0;
+			top: 50%;
+			transform: translateY(-50%);
+		}
+		.list{
+			box-sizing: border-box;
+			overflow: hidden;
+			.item{
+				width: 33.3333%;
+				float: left;
+				text-align: center;
+				box-sizing: border-box;
+				padding:20upx;
+				.icon{
+					width: 60upx;
+					height: 60upx;
+					margin: 20upx 0;
+				}
+				.title{
+					font-size: 28upx;
+					color: #333;
+				}
+				.describe{
+					font-size: 20upx;
+					color: #999;
+				}
+			}
+		}
+	}
+}
+</style>
---
+++ pages/tabbar/tabbar2/tabbar2.vue
@@ -0,0 +1,98 @@
+<template>
+	<view class="content">
+		<view class="tabber">
+			<view :class="index==0?'item active':'item'" @click="tabchange(0)">微信号</view>
+			<view :class="index==1?'item active':'item'" @click="tabchange(1)">微信群</view>
+		</view>
+		<view class="list">
+			<view class="item" v-for="(v,index) in number" :key="index">
+				<view class="body">
+					<image src="../../../static/logo.png" mode="" class="icon"></image>
+					<view class="name">曾老师</view>
+					<view class="describe">描述</view>
+					<button type="primary" plain="true" size="mini">查看</button>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				index:0,
+				number:6
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			tabchange(idx){
+				this.index=idx
+				if(idx==1){
+					this.number=12
+				}else{
+					this.number=6
+				}
+			}
+		},
+		onPullDownRefresh() {
+			let self = this
+			setTimeout(function () {
+		        uni.stopPullDownRefresh();
+				self.index=0
+				self.number=6
+		    }, 1000);
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	page{background-color: #f5f5f5;}
+	.content {
+		.tabber{
+			position: fixed;
+			top: 80upx;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			width:100%;
+			height:88upx;
+			box-sizing: border-box;
+			border-bottom:1px solid #f7f7f7;
+			z-index:99;
+			.item{
+				font-size: 32upx;
+				flex: 1;
+				text-align: center;
+				line-height: 88upx;
+				background-color: #fff;
+			}
+			.item.active{
+				color: #007AFF;
+				font-weight: bold;
+			}
+		}
+		.list{
+			overflow: hidden;
+			box-sizing: border-box;
+			margin-top:88upx;
+			.item{
+				width: 33.3333%;
+				overflow: hidden;
+				box-sizing: border-box;
+				padding: 20upx;
+				float: left;
+				.body{
+					background: #fff;
+					.icon{
+						width: 100%;
+						height: 220upx;
+					}
+				}
+			}
+		}
+	}
+</style>
